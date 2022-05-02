d1fef15e0891300cdff400154fa11b95040632e4
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-05-01 22:37:58 -0700

    added router to application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import HelloMessage from './components/App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 document.addEventListener('DOMContentLoaded', () => {
-  root.render(<HelloMessage name="World" />);
+  root.render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
 });
 
 /* eslint no-console:0 */
